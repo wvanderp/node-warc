376c6cd6ea1770d2b39f466894a0590b1c56c52c
wouter van der plas <wouter van der plas>
2016-03-09 23:24:36 +0100

    making progress with the struction of the project

--- run.js
+++ run.js
@@ -1,19 +1,3 @@
-//==========
-// start test code
-var config = require('./config.json');
-var options = config.options;
+import {Warc} from "./warc";
 
-var warc = require("./warc.js");
-
-warc.startWarc(options);
-
-var request = {
-    "User-Agent": "ArchiveTeam ArchiveBot/20131009.01",
-    "Accept": "*/*",
-    "Host": "study.gaijinpot.com",
-    "Connection": "Keep-Alive"
-};
-
-var methode = "GET / HTTP/1.1";
-
-warc.addrequest(request, methode);
+var warc = new Warc();
